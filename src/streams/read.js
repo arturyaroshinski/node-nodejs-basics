a678d4872fad0caf6acaa25b6f468c4e256a0c33
Artur Yaroshinski <arturyaroshinskiy@gmail.com>
2022-12-05 18:27:49 +0300

    feat: implement stream functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import fs from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const read = async () => {
-    // Write your code here 
+    const rs = fs.createReadStream(join(__dirname, "files", "fileToRead.txt"));
+    rs.pipe(process.stdout);
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { stdin, stdout } from "process";
+import { Transform, pipeline } from "stream";
+import { EOL } from "os";
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+            const chunkStr = chunk.toString();
+            const reversed = chunkStr
+                .replace(EOL, "")
+                .split("")
+                .reverse()
+                .join("");
+
+            callback(null, reversed + EOL);
+        },
+    });
+
+    pipeline(stdin, reverse, stdout, (error) => console.log(error));
 };
 
 await transform();
