dc1d4ddaa3362c8b7cef47c4c39e9d9440fec7b7
Artur Yaroshinski <arturyaroshinskiy@gmail.com>
2022-12-05 18:30:19 +0300

    feat: implement zlib functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import zlib from "zlib";
+import fs from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const compress = async () => {
-    // Write your code here 
+    const rs = fs.createReadStream(
+        join(__dirname, "files", "fileToCompress.txt")
+    );
+    const ws = fs.createWriteStream(join(__dirname, "files", "archive.gz"));
+    const gzip = zlib.createGzip();
+
+    rs.pipe(gzip).pipe(ws);
 };
 
 await compress();
