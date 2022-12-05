4518a332f99ee64f449fa0e00030a2540de5e981
Artur Yaroshinski <arturyaroshinskiy@gmail.com>
2022-12-05 18:23:43 +0300

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import crypto from "crypto";
+import fs from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const calculateHash = async () => {
-    // Write your code here 
+    const fileBuffer = await fs.readFile(
+        join(__dirname, "files", "fileToCalculateHashFor.txt")
+    );
+
+    const hash = crypto.createHash("sha256").update(fileBuffer).digest("hex");
+
+    console.log(hash);
 };
 
 await calculateHash();
