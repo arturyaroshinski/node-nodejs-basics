03e4042430f07316fbb939c20d0325e5276bc90b
Artur Yaroshinski <arturyaroshinskiy@gmail.com>
2022-12-05 18:33:41 +0300

    feat: implement worker threads functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import os from "os";
+import { Worker } from "worker_threads";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const performCalculations = async () => {
-    // Write your code here
+    const cpus = os.cpus();
+
+    const result = await Promise.allSettled(
+        cpus.map(
+            (_, idx) =>
+                new Promise((res, rej) => {
+                    const worker = new Worker(join(__dirname,"worker.js"), {
+                        workerData: 10 + idx,
+                    });
+
+                    worker.on("message", (data) =>
+                        res({ status: "resolved", data })
+                    );
+                    worker.on("error", rej);
+                })
+        )
+    );
+
+    const transformedResult = result.map(
+        (res) => res.value ?? { status: "error", data: null }
+    );
+
+    console.log(transformedResult);
 };
 
 await performCalculations();
